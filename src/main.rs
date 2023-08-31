37dafa7066d650d26a42855703b1cdec3c9e53c4
Rowan Clarke <r.n.clarke@lancaster.ac.uk>
2023-08-31 20:36:54 +0100

    Deserialize Configuration #6

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use serde::Deserialize;
+use std::{fs::File, io::Read, path::PathBuf, str::FromStr};
+use toml::{from_str, Table};
+
+struct Workspace {
+    root: PathBuf,
+}
+
+impl Workspace {
+    fn get_config(&self) -> Configuration {
+        let mut config = self.root.clone();
+        config.push(".remedy");
+        config.push("config.toml");
+        let mut config = File::open(config).expect("Error opening config");
+        let mut buffer = String::new();
+        config.read_to_string(&mut buffer).unwrap();
+        from_str::<Configuration>(&buffer).expect("Error parsing configuration")
+    }
+}
+
+#[derive(Deserialize, Debug)]
+struct Configuration {
+    profile: Vec<Profile>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Profile {
+    grammar: PathBuf,
+    rule: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let workspace = Workspace {
+        root: PathBuf::from_str("/home/rowan/wkspc").unwrap(),
+    };
+    println!("{:?}", workspace.get_config());
 }
